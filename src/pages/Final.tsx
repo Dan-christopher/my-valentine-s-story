a9cdda72267e4e8d1b29b55e260da59962aca07e
Dan Christopher <danchristopher12751@gmail.com>
2026-02-06 15:15:24 +0530

    added some extra photos

--- src/pages/Final.tsx
+++ src/pages/Final.tsx
@@ -23,7 +23,19 @@ const Final = () => {
     "/images/photo2.jpeg",
     "/images/photo3.jpeg",
     "/images/photo4.jpeg",
-    "/images/photo5.jpeg"
+    "/images/photo5.jpeg",
+    "/images/photo6.jpeg",
+    "/images/photo7.jpeg",
+    "/images/photo8.jpeg",
+    "/images/photo9.jpeg",
+    "/images/photo10.jpeg",
+    "/images/photo11.jpeg",
+    "/images/photo12.jpeg",
+    "/images/photo13.jpeg",
+    "/images/photo14.jpeg",
+    "/images/photo15.jpeg",
+    "/images/photo16.jpeg",
+    "/images/photo17.jpeg"
   ];
 
   // Set initial volume when component mounts
